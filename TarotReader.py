15485a5969711e05b12ab5fc9a122ce25de046b6
CodeofGuff <guffcode@gmail.com>
2024-06-06 17:21:44 -0700

    Shortened code

--- TarotReader.py
+++ TarotReader.py
@@ -17,16 +17,34 @@ tarot = {
     1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"
     }
 
-rand_num_past = random.choice(list(tarot.keys()))
-rand_num_present = random.choice(list(tarot.keys()))
-rand_num_future = random.choice(list(tarot.keys()))
+selection = random.sample(list(tarot.keys()), 3)
+spread = {"past": tarot.pop(selection[0]), "present": tarot.pop(selection[1]),"future": tarot.pop(selection[2])}
 
-spread = {}
 
-spread["past"] = tarot.pop(rand_num_past)
-spread["present"] = tarot.pop(rand_num_present)
-spread["future"] = tarot.pop(rand_num_future)
 print(spread)
 
 for read, card in spread.items():
     print(f"Your", read, "is the", card, "card.")
+
+
+
+
+
+
+#### MOOT CODE ####
+# Generates random number from the tarot list to be used as the selected card. 
+##rand_num_past = random.choice(list(tarot.keys()))
+##rand_num_present = random.choice(list(tarot.keys()))
+##rand_num_future = random.choice(list(tarot.keys()))
+
+
+# fixes Key Error, if a selected tarot list number would be the same as a previous 
+##while rand_num_present == rand_num_past:
+    ##rand_num_present = random.choice(list(tarot.keys()))
+
+##while rand_num_future == rand_num_past or rand_num_future == rand_num_past:
+    ##rand_num_future = random.choice(list(tarot.keys()))
+
+##spread["past"] = tarot.pop(rand_num_past)
+##spread["present"] = tarot.pop(rand_num_present)
+##spread["future"] = tarot.pop(rand_num_future) 
